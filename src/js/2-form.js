0eaf0e748be11d5a5d1dd3c72822dde183ca3b66
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-08-04 15:28:30 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -9,3 +9,23 @@ form.addEventListener('input', (event) => {
     formData[event.target.name] = event.target.value;
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
+  window.addEventListener('DOMContentLoaded', () => {
+    const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (savedFormData) {
+      formData = savedFormData;
+      document.querySelector('form').elements.email.value = savedFormData.email;
+      document.querySelector('form').elements.message.value = savedFormData.message;
+    }
+  });
+  
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!formData.email || !formData.message) {
+      alert('Fill please all fields');
+    } else {
+      console.log(formData);
+      localStorage.removeItem('feedback-form-state');
+      formData = { email: "", message: "" };
+      document.querySelector('form').reset();
+    }
+  });
