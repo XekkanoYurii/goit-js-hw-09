dc6648e354f4d2f263556d810ee31a5814773621
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-08-04 13:25:46 +0000

    Deploying to gh-pages from @ XekkanoYurii/goit-js-hw-09@11bf6ae6fe1639cd396c1b30988f600c88da9c6d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const t={email:"",message:""},a=document.querySelector(".feedback-form");a.addEventListener("input",e=>{t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))});
+//# sourceMappingURL=commonHelpers2.js.map
